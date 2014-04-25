f7c1c604877a9f2e7943127a0926c8abd53d1d98
wwliu <w.liu@gitp.nl>
2014-04-25 23:21:30 +0200

    Made changes to peer assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,19 @@
 ## These two functions allow the user to create a matrix,
-## cache (store) that matrix and compute the inverse of the matrix
+## cache (store) that matrix and compute the inverse of it
+## Note: only square matrices can be inverted
 
 
 ## This function is called from the function "cacheSolve" and
-## creates and caches the matrix
+## creates and caches the matrix to be inverted
 makeCacheMatrix <- function(x = matrix()) {
  makeMatrix <<- matrix(rnorm(x^2), x, x)
  print(makeMatrix)
 }
 
 
-## Type "cacheSolve" (where x is between 2 and 9 including)
-## This creates and a square matrix (only square matrices can be inverted),
-## invert it at store in under the name "invertedMatrix"
+## Type "cacheSolve(x)" (where x is between 2 and 9, including)
+## This creates a square matrix using the above function,
+## invert it using this function and store it under the name "invertedMatrix"
 cacheSolve <- function(x, ...) {
        if (!(x %in% 2:9)) stop ("x must be 2 through 9")
        makeCacheMatrix(x)
